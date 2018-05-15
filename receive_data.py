4fef38e143562c908944d9060858669852e75e1a
Jangmin <pulse9.dev4@gmail.com>
2018-05-15 04:15:45 +0000

    FstPhoto

---
+++ receive_data.py
@@ -0,0 +1,38 @@
+import os
+import threading
+import subprocess
+import time
+
+
+def receive_data():
+  print("doing something")
+  storage_list = os.popen('sudo gsutil ls -l gs://pulse9-camera.appspot.com/Images/').read()
+  tmp = storage_list.split()
+  for k in tmp:
+    if k.find("gs") != -1 and k.find("REALISTIC") != -1:
+      os.system("sudo gsutil cp " + k + " ./get_data/")
+      os.system("sudo gsutil mv " + k + " gs://pulse9-camera.appspot.com/Save/")
+      k = k.split("/")[-1]
+      if ".jpg" not in k:
+        k_ = k + ".jpg"
+        subprocess.call("mv ./get_data/" + k + " ./get_data/" + k_,
+                       shell=True)
+      else:
+        k_ = k
+      
+  outfiles = os.listdir("./out_data/")
+  for file in outfiles:
+    print(file)
+    file_ = file.replace("@", ".")
+    splits = file_.split("_")[1]
+    dirname = file_[file_.rfind("_")-1:]
+    j=file_.split("_")[-1]
+    os.system("sudo gsutil cp ./out_data/"+file +" gs://pulse9-camera.appspot.com/Result/"+splits+"/"+j)
+    os.remove("./out_data/"+file)
+
+  threading.Timer(5, receive_data).start()
+
+if not os.path.exists("./get_data"):
+  os.makedirs("./get_data")
+receive_data()
+
---
+++ test.py
@@ -0,0 +1,37 @@
+import os
+import threading
+import numpy as np
+import subprocess
+import time
+import skimage.io
+import cv2
+from PIL import Image
+
+
+received_dir = "./get_data/"
+
+
+def test():
+  print("processing ...")
+  
+  files = os.listdir(received_dir)
+  for file in files:
+    if ".jpg" in file:
+      print("file name: {}".format(file))
+      people = skimage.io.imread(received_dir+file)
+      people = cv2.imread(received_dir+file)
+      skimage.io.imsave(received_dir+file, people)
+      subprocess.call("sudo rm ./images/contents/*.jpg", shell=True)
+      subprocess.call("sudo rm ./results/*", shell=True)
+      #subprocess.call("sudo convert -resize 50% ./get_data/"+file+" ./get_data/"+file, shell=True)
+      img = Image.open("./get_data/"+file)
+      img = img.resize((406,722), Image.ANTIALIAS)
+      img.save("./get_data/"+file)
+      subprocess.call("cp ./get_data/"+file+" ./images/contents/", shell=True)
+      subprocess.check_call("python demo.py --content_image_path ./images/contents/"+file+" --style_image_path ./images/styles/"+file.split('_')[0] + ".jpg --output_image_path ./results/" + file , shell=True)
+      img=skimage.io.imread("./results/"+file)
+      skimage.io.imsave("./out_data/"+file,img)
+      subprocess.call("rm ./get_data/"+file, shell=True)
+    
+  threading.Timer(5, test).start()
+test()
